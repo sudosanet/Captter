bb5ee16d3075c6a3bc417078e11883dfbae65fc7
かひわし4（バージョン1） <khws4v1@gmail.com>
2015-03-14 16:37:05 +0900

    tweak

--- captter3/Core/imagesearch.cs
+++ captter3/Core/imagesearch.cs
@@ -14,9 +14,7 @@ namespace captter3
             {
                 System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(Properties.Settings.Default.pass);
                 IEnumerable<System.IO.FileInfo> fileList = dir.GetFiles("*.*", System.IO.SearchOption.TopDirectoryOnly);
-                string ex = null;
-
-                ex = Properties.Settings.Default.img;
+                string ex = Properties.Settings.Default.img;
 
                 IEnumerable<System.IO.FileInfo> fileQuery =
                     from file in fileList
