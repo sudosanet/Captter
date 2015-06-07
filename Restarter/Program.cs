75c655fe95b725d9578a16cbd93e36f8cfaa6b89
sudosan <cream.stew.007+586@gmail.com>
2015-06-07 11:01:11 +0900

    山田うどん
    開発版をmasterブランチに上げる人間の屑

---
+++ Restarter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ファイルをコピーします。何かのキーを押してください");
+            Console.ReadKey();
+            string path = System.Environment.CurrentDirectory + "\\tmp\\";
+            string copy = "copying...";
+            string ex = "captter3.exe";
+            Console.WriteLine(copy+ex);
+            System.IO.File.Copy(path + ex, System.Environment.CurrentDirectory + ex, true);
+            Console.WriteLine(copy + ex);
+            ex = "captter3.exe.config";
+            System.IO.File.Copy(path + ex, System.Environment.CurrentDirectory + ex, true);
+            Console.WriteLine(copy + ex);
+            ex = "CoreTweet.dll";
+            System.IO.File.Copy(path + ex, System.Environment.CurrentDirectory + ex, true);
+            Console.WriteLine(copy + ex);
+            ex = "CoreTweet.xml";
+            System.IO.File.Copy(path + ex, System.Environment.CurrentDirectory + ex, true);
+            Console.WriteLine(copy + ex);
+            ex = "Newtonsoft.Json.dll";
+            System.IO.File.Copy(path + ex, System.Environment.CurrentDirectory + ex, true);
+            Console.WriteLine(copy + ex);
+            ex = "Newtonsoft.Json.xml";
+            System.IO.File.Copy(path + ex, System.Environment.CurrentDirectory + ex, true);
+            System.IO.File.Delete(System.Environment.CurrentDirectory + "\\tmp");
+            System.Diagnostics.Process.Start("captter3.exe");
+            Environment.Exit(0);
+        }
+    }
+}
--- captter3/updater.xaml.cs
+++ captter3/updater.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.IO;
+using System.IO.Compression;
 
 namespace captter3
 {
@@ -30,16 +32,71 @@ namespace captter3
             get { return _url; }
             set { _url = value; }
         }
+        System.Net.WebClient down = null;
+        string uppath = System.Environment.CurrentDirectory + "\\tmp\\update.zip";
+        string path = System.Environment.CurrentDirectory + "\\tmp";
         public void download()
         {
+            Uri uri = new Uri(url);
+            
+            if (System.IO.File.Exists(path)){ }
+            else
+            {
+                System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(path);
+                di.Create();
+            }
+            if (down == null)
+            {
+                down = new System.Net.WebClient();
+                down.DownloadProgressChanged +=
+                    new System.Net.DownloadProgressChangedEventHandler(sinchoku);
+                down.DownloadFileCompleted +=
+                    new System.ComponentModel.AsyncCompletedEventHandler(comp);
+            }
+            down.DownloadFileAsync(uri, uppath);
+        }
+        private void sinchoku(object sender,
+        System.Net.DownloadProgressChangedEventArgs e)
+        {
+            downloading.Visibility = Visibility.Visible;
+            pb.Value = e.ProgressPercentage;
+            downloading.Content = "ファイルをダウンロードしています("+e.ProgressPercentage+"%)・・・";
+        }
 
-            //ダウンロードしたファイルの保存先
-            string fileName = System.Environment.CurrentDirectory + "\\tmp\\update.zip";
-            Console.WriteLine(url);
-            Console.WriteLine(fileName);
-            System.Net.WebClient down = new System.Net.WebClient();
-            down.DownloadFile(url, fileName);
-            down.Dispose();
+        private void comp(object sender,
+            System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                MessageBox.Show("キャンセルされました。");
+                this.Close();
+            }
+            else if (e.Error != null)
+            {
+                MessageBox.Show("エラー:{0}", e.Error.Message);
+                this.Close();
+            }
+            else
+            {
+                Console.WriteLine("ダウンロードが完了しました。");
+                zip();
+            }
+
+        }
+
+        private void restart_Click(object sender, RoutedEventArgs e)
+        {
+            if (down != null)
+                down.CancelAsync();
+        }
+        private void zip()
+        {
+            ext.Visibility = Visibility.Visible;
+            ZipFile.ExtractToDirectory(uppath, path);
+            System.IO.File.Delete(uppath);
+
+            System.Diagnostics.Process.Start("Restarter.exe");
+            Environment.Exit(0);
         }
     }
 }
