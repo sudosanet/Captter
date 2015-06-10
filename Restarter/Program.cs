65706814fd9184c897c2b18a1bf93f9066d5b712
かひわし4（バージョン1） <khws4v1@gmail.com>
2015-06-10 21:15:22 +0900

    update restarter

--- Restarter/Program.cs
+++ Restarter/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Restarter
 {
@@ -13,38 +14,26 @@ namespace Restarter
             System.Threading.Thread.Sleep(10);
             string path = System.Environment.CurrentDirectory + "\\tmp\\";
             string copy = "copying...";
-            string ex = "captter3.exe";
-            Console.WriteLine(copy+ex);
-            try
+
+            foreach(string fileName in Directory.GetFiles(path))
             {
-                System.IO.File.Copy(path + ex, System.Environment.CurrentDirectory + "\\" + ex, true);
-                Console.WriteLine(copy + ex);
-                ex = "captter3.exe.config";
-                System.IO.File.Copy(path + ex, System.Environment.CurrentDirectory + "\\" + ex, true);
-                Console.WriteLine(copy + ex);
-                ex = "CoreTweet.dll";
-                System.IO.File.Copy(path + ex, System.Environment.CurrentDirectory + "\\" + ex, true);
-                Console.WriteLine(copy + ex);
-                ex = "CoreTweet.xml";
-                System.IO.File.Copy(path + ex, System.Environment.CurrentDirectory + "\\" + ex, true);
-                Console.WriteLine(copy + ex);
-                ex = "Newtonsoft.Json.dll";
-                System.IO.File.Copy(path + ex, System.Environment.CurrentDirectory + "\\" + ex, true);
-                Console.WriteLine(copy + ex);
-                ex = "Newtonsoft.Json.xml";
-                System.IO.File.Copy(path + ex, System.Environment.CurrentDirectory + "\\" + ex, true);
+                try
+                {
+                    Console.WriteLine(copy + fileName);
+                    System.IO.File.Copy(fileName, System.Environment.CurrentDirectory);
+                }
+                catch (System.IO.DirectoryNotFoundException)
+                {
+                    System.Windows.Forms.MessageBox.Show("このアプリケーションはシステムの更新時に自動実行されます。手動で実行する必要はありませんが削除しないでください");
+                    Environment.Exit(1);
+                }
+                catch (System.IO.IOException)
+                {
+                    System.Threading.Thread.Sleep(100);
+                    Environment.Exit(2);
+                }
             }
-            catch(System.IO.DirectoryNotFoundException)
-            {
-                System.Windows.Forms.MessageBox.Show("このアプリケーションはシステムの更新時に自動実行されます。手動で実行する必要はありませんが削除しないでください");
-                Environment.Exit(0);
-            }
-            catch(System.IO.IOException)
-            {
-                System.Threading.Thread.Sleep(100);
-                return;
-            }
-            System.IO.Directory.Delete(System.Environment.CurrentDirectory + "\\tmp",true);
+            System.IO.Directory.Delete(path,true);
             System.Windows.Forms.MessageBox.Show("アップデートが完了しました。");
             System.Diagnostics.Process.Start("captter3.exe");
             Environment.Exit(0);
