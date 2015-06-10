d71777ca4206d843af55c3c5b0bab978b238b3a8
かひわし4（バージョン1） <khws4v1@gmail.com>
2015-06-10 21:43:10 +0900

    add error message

--- Restarter/Program.cs
+++ Restarter/Program.cs
@@ -9,29 +9,41 @@ namespace Restarter
 {
     class Program
     {
+        static void Copy(string fileName)
+        {
+            try
+            {
+                Console.WriteLine("copying..." + fileName);
+                System.IO.File.Copy(fileName, System.Environment.CurrentDirectory);
+            }
+            catch (System.IO.DirectoryNotFoundException)
+            {
+                System.Windows.Forms.MessageBox.Show("このアプリケーションはシステムの更新時に自動実行されます。手動で実行する必要はありませんが削除しないでください");
+                Environment.Exit(1);
+            }
+            catch (System.IO.IOException)
+            {
+                if (System.Windows.Forms.MessageBox.Show(fileName + "のコピーに失敗しました。", "エラー",
+                    System.Windows.Forms.MessageBoxButtons.RetryCancel) == System.Windows.Forms.DialogResult.Retry)
+                {
+                    System.Threading.Thread.Sleep(100);
+                    Copy(fileName);
+                }
+                else
+                {
+                    Environment.Exit(2);
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             System.Threading.Thread.Sleep(10);
             string path = System.Environment.CurrentDirectory + "\\tmp\\";
-            string copy = "copying...";
 
             foreach(string fileName in Directory.GetFiles(path))
             {
-                try
-                {
-                    Console.WriteLine(copy + fileName);
-                    System.IO.File.Copy(fileName, System.Environment.CurrentDirectory);
-                }
-                catch (System.IO.DirectoryNotFoundException)
-                {
-                    System.Windows.Forms.MessageBox.Show("このアプリケーションはシステムの更新時に自動実行されます。手動で実行する必要はありませんが削除しないでください");
-                    Environment.Exit(1);
-                }
-                catch (System.IO.IOException)
-                {
-                    System.Threading.Thread.Sleep(100);
-                    Environment.Exit(2);
-                }
+                Copy(fileName);
             }
             System.IO.Directory.Delete(path,true);
             System.Windows.Forms.MessageBox.Show("アップデートが完了しました。");
